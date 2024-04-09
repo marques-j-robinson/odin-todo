f294741fb56665147fe6082e067f78a3f9db1c1b
marques-j-robinson <marques.j.robinson@gmail.com>
2024-04-09 10:35:06 -0700

    add logic and display handling for updating project details

--- src/projects.js
+++ src/projects.js
@@ -10,21 +10,65 @@ const createDetailsEl = ({name, description}) => {
     nameEl.innerText = name
     el.append(nameEl)
 
+    const nameInputEl = document.createElement('input')
+    nameInputEl.classList.add('project__name-input')
+    nameInputEl.classList.add('hide')
+    nameInputEl.placeholder = name
+    el.append(nameInputEl)
+
     const descriptionEl = document.createElement('p')
     descriptionEl.classList.add('project__description')
     descriptionEl.innerText = description
     el.append(descriptionEl)
 
+    const descriptionInputEl = document.createElement('textarea')
+    descriptionInputEl.classList.add('project__description-input')
+    descriptionInputEl.classList.add('hide')
+    descriptionInputEl.placeholder = description
+    el.append(descriptionInputEl)
+
     return el
 }
 
-const createActionsEl = id => {
+const createActionsEl = (id, detailsEl) => {
     const el = document.createElement('div')
     el.classList.add('project__actions')
+
+    const nameEl = detailsEl.querySelector('.project__name')
+    const nameInputEl = detailsEl.querySelector('.project__name-input')
+    const descriptionEl = detailsEl.querySelector('.project__description')
+    const descriptionInputEl = detailsEl.querySelector('.project__description-input')
+
+    const saveBtn = document.createElement('button')
+    saveBtn.innerText = 'Save'
+    saveBtn.classList.add('hide')
+    el.append(saveBtn)
 
     const editBtn = document.createElement('button')
     editBtn.innerText = 'Edit'
     el.append(editBtn)
+
+    saveBtn.addEventListener('click', () => {
+        console.log(nameInputEl.value)
+        console.log(nameInputEl.placeholder)
+        projects[id] = {
+            ...projects[id],
+            name: nameInputEl.value || nameInputEl.placeholder,
+            description: descriptionInputEl.value || descriptionInputEl.placeholder,
+        }
+
+        displayAllProjects(projects)
+    })
+
+    editBtn.addEventListener('click', () => {
+        editBtn.classList.add('hide')
+        nameEl.classList.add('hide')
+        descriptionEl.classList.add('hide')
+
+        saveBtn.classList.remove('hide')
+        nameInputEl.classList.remove('hide')
+        descriptionInputEl.classList.remove('hide')
+    })
 
     const deleteBtn = document.createElement('button')
     deleteBtn.innerText = 'Delete'
@@ -74,7 +118,7 @@ const createProjectEl = (projectDetails, projectId) => {
     el.classList.add('project')
 
     const detailsEl = createDetailsEl(projectDetails)
-    const actionsEl = createActionsEl(projectId)
+    const actionsEl = createActionsEl(projectId, detailsEl)
     const headerEl = createHeaderEl(detailsEl, actionsEl)
     el.append(headerEl)
 
